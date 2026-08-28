01d4642b595b427e423886199dadd53751d4f08d
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-08-28 07:04:16 -0500

    Migrate Kotlin JVM target DSL

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -22,7 +24,12 @@ android {
     }
 
     buildFeatures { compose = true }
-    kotlinOptions { jvmTarget = "17" }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 dependencies {

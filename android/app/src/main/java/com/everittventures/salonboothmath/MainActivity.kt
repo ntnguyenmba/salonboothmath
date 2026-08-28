a2d274eb7524a3fa28129feea9bf2e37f212836a
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-08-28 17:13:38 -0500

    Persist selected Android app language

--- android/app/src/main/java/com/everittventures/salonboothmath/MainActivity.kt
+++ android/app/src/main/java/com/everittventures/salonboothmath/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.everittventures.salonboothmath
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.material3.MaterialTheme
+import androidx.core.os.LocaleListCompat
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var billing: BillingManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val prefs = getSharedPreferences("salon_booth_math", MODE_PRIVATE)
+        val savedLanguage = prefs.getString("app_language", "en") ?: "en"
+        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(savedLanguage))
+
         super.onCreate(savedInstanceState)
         billing = BillingManager(this).also { it.start() }
         setContent {

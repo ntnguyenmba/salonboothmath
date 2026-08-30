bac02db9f83d16fe03ba6800235bc6554227df89
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-08-29 19:51:59 -0500

    Point Settings at shared legal URLs and add hybrid to Android Compare

---
+++ android/app/src/main/java/com/everittventures/salonboothmath/LegalUrls.kt
@@ -0,0 +1,7 @@
+package com.everittventures.salonboothmath
+
+object LegalUrls {
+    const val privacy = "https://sites.google.com/everittventures.com/salon-booth-math/privacy"
+    const val terms = "https://sites.google.com/everittventures.com/salon-booth-math/terms"
+    const val support = "https://sites.google.com/everittventures.com/salon-booth-math/support"
+}

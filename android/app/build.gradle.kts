b24092bc41595c75f41c24d9e2252cec5b75a28e
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-09-20 22:21:01 -0500

    Add Google Mobile Ads Android SDK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.glance:glance-appwidget:1.2.0")
     implementation("com.android.billingclient:billing-ktx:9.1.0")
+    implementation("com.google.android.gms:play-services-ads:25.5.0")
     testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }

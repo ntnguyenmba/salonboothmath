2eddca6ad10b1b0995114c457ffb3a1e2f2148b2
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-09-20 22:15:54 -0500

    Add free user AdMob banner view

---
+++ ios/SalonBoothMath/AdMobBannerView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import GoogleMobileAds
+import UIKit
+
+enum AdMobConfig {
+    static let bannerAdUnitID = "ca-app-pub-1237434632796366/2950294014"
+}
+
+struct FreeBannerAdView: UIViewRepresentable {
+    func makeUIView(context: Context) -> BannerView {
+        let banner = BannerView(adSize: AdSizeBanner)
+        banner.adUnitID = AdMobConfig.bannerAdUnitID
+        banner.rootViewController = UIApplication.shared.connectedScenes
+            .compactMap { ($0 as? UIWindowScene)?.keyWindow?.rootViewController }
+            .first
+        banner.load(Request())
+        return banner
+    }
+
+    func updateUIView(_ uiView: BannerView, context: Context) {}
+}

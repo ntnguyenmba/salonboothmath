b621b22a37b481063025f49e2c1997453616ce0e
Thuy Nguyen <ntnguyenmba@gmail.com>
2026-09-25 16:10:30 -0500

    Bump Android release to 1.1.0 build 3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.everittventures.salonboothmath"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0"
     }
 
     compileOptions {
